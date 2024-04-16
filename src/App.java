8ef620ed04d72d898bcfb81b40d0e2f05e71ef8f
Leo Lao <lao.leo58@gmail.com>
2024-04-16 11:14:13 -0400

    Added comments to code

--- src/App.java
+++ src/App.java
@@ -1,15 +1,19 @@
 //importing javax swing JOptionPane Class
 import javax.swing.JOptionPane;
 
-
+//Start of App Class
 public class App {
+    //Start of Main Method
     public static void main(String[] args) throws Exception {
+        //Prompting User for their name
         String name = JOptionPane.showInputDialog("Enter your name: ");
         JOptionPane.showMessageDialog(null, "Oh Hello.." +name);
 
+        //Prompting user for their age
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
         JOptionPane.showMessageDialog(null, "Wow, you are "+age+" years old");
         
+        //Repeatedly asking them a question until they answer correctly
         int answer = 0;
         while(answer!=1){
             answer = JOptionPane.showConfirmDialog(null,"Is the sky blue?", "Truly Tragic Some Would Say",JOptionPane.YES_NO_OPTION);
@@ -20,5 +24,8 @@ public class App {
                 JOptionPane.showMessageDialog(null,"Damn "+name+", you must've just come from stupid town");
             }
         }
-    }
-}
+        
+        //Calling the numberGUess method
+        GuessNumber.numberGuess();
+    }//End of Main Method
+}//End of App Class
--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,6 +1,27 @@
+//importing javax swing JOptionPane Class
+import javax.swing.JOptionPane;
+
+//Start of GuessNumber class
 public class GuessNumber {
+    //Start of numberGuess method
     public static void numberGuess(){
+        //Initializing the number that is being guessed
         int realNumber = 84;
-        int guessNumber = Integer.partInt(JOptionPane.showInputDialog)
-    }
-}
+        //Getting the intial guess from the user
+        int guessNumber = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1 and 100"));
+        
+        //Continually prompting the user for a guess until they guess the correct number. Providing feedback on their guess
+        while(guessNumber!=realNumber){
+            //JOptionPane.showMessageDialog(null, guessNumber+" is not the correct number, guess again.");
+            if(guessNumber > realNumber){
+                JOptionPane.showMessageDialog(null, guessNumber+" is too high");
+            }
+            else if(guessNumber < realNumber){
+                JOptionPane.showMessageDialog(null, guessNumber+" is too low");
+            }
+            guessNumber = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1 and 100"));
+        }
+        //When they guess correctly, giving them a congratulatory message
+        JOptionPane.showMessageDialog(null, "Correct, "+realNumber+" is the number.");
+    }//End of numberGuess method
+}//End of GuessNumber class
